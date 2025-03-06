222e0a12b7bfed0494fcc61bf1cba615f701675e
Waleed Sadeh <140826450+waleedsadeh@users.noreply.github.com>
2025-03-06 10:25:29 +0000

    my changes

--- src/api/actions.ts
+++ src/api/actions.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from "axios";
 
-const API_URL = "https://studious-train-7pq794x5gr72xrrv-3000.app.github.dev/api";
+const API_URL = "https://glorious-carnival-wr79j964xrgw2vvx4-3000.app.github.dev/api";
 
 export const getWeatherData = async (city: string): Promise<WeatherData> => {
   return new Promise<WeatherData>((resolve, reject) => {
